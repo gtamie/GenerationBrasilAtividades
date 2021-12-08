c06e71bb3742d7d2c2b2d581e0d40edcd91286a7
gtamie <89866151+gtamie@users.noreply.github.com>
2021-12-08 17:06:58 -0300

    exercícios 3 e 4

---
+++ src/atv1_ProgramacaoSequencial/Exercicio3.java
@@ -0,0 +1,20 @@
+package atv1_ProgramacaoSequencial;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		int evento, h, m, s, r;
+		Scanner leia = new Scanner (System.in);
+		System.out.print("Digite o tempo de duração em segundos: ");
+		evento=leia.nextInt();
+		h=evento/3600;
+		r=evento%3600;
+		m=r/60;
+		s=r%60;
+		System.out.print("\nO tempo de duração é "+h+" horas "+m+" minutos e "+s+" segundos");
+
+	}
+
+}
---
+++ src/atv1_ProgramacaoSequencial/Exercicio4.java
@@ -0,0 +1,27 @@
+package atv1_ProgramacaoSequencial;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+		double a, b, c, r, s, d;
+		Scanner leia = new Scanner (System.in);
+		System.out.print("Digite o valor de A: ");
+		a=leia.nextInt();
+		
+		System.out.print("Digite o valor de B: ");
+		b=leia.nextInt();
+		
+		System.out.print("Digite o valor de C: ");
+		c=leia.nextInt();
+		
+		r= Math.pow(a+b,2.0);	
+		s= Math.pow(b+c,2.0);
+		d= (r+s)/2;
+		System.out.print("O resultado é: "+d);
+
+	}
+
+}
