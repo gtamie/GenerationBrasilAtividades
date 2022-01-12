307c91af2c96de97570ce439428ece16e898269b
Giuliana <giulianatamie@gmail.com>
2022-01-12 19:48:38 -0300

    atualizado

--- mysql/atividade2.sql
+++ mysql/atividade2.sql
@@ -11,19 +11,19 @@ disponibilidade boolean,
 primary key (id)
 );
 
-insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("smartphone xmas", 1000.00, "eletrônicos",1);
-insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("smartwatch xmas3000", 800.00, "eletrônicos",0);
-insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("pacote 5 camisetas", 150.00, "vestuário",1);
-insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("calça mom jeans", 70.00, "vestuario",1);
-insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("smartphone xmas xx", 2000.00, "eletrônicos",0);
-insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("jaqueta bomber azul", 100.00, "vestuário",1);
-insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("jaqueta bomber dourada", 119.99, "vestuário",1);
-insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("saia midi", 79.99, "vestuário",1);
+insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("smartphone xmas", 1000.00, "eletrônicos",true);
+insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("smartwatch xmas3000", 800.00, "eletrônicos",false);
+insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("pacote 5 camisetas", 150.00, "vestuário",true);
+insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("calça mom jeans", 70.00, "vestuario",true);
+insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("smartphone xmas xx", 2000.00, "eletrônicos",false);
+insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("jaqueta bomber azul", 100.00, "vestuário",true);
+insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("jaqueta bomber dourada", 119.99, "vestuário",true);
+insert into tb_produtos (nome, preco, categoria, disponibilidade) values ("saia midi", 79.99, "vestuário",true);
 
 select * from tb_produtos where preco>500;
 
 select * from tb_produtos where preco<500;
 
-update tb_produtos set disponibilidade=0 where id=1;
+update tb_produtos set disponibilidade=false where id=1;
 
 select * from tb_produtos;
